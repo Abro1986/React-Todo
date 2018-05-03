7a43f2ac5480e49c1e9228da09bda4f1712f9d9e
Abro1986 <AndrewBroestl@gmail.com>
2018-05-03 11:26:30 -0600

    One commit to rul ethem all

---
+++ my-react-todo/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//import './App.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+      	<p>this is the home</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
